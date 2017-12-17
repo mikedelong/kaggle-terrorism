e5c797e09c01fed5af6a7526ef6b623358a4109e
Mike DeLong <mikedelong@outlook.com>
2017-12-17 14:58:51 -0500

    refactor: extract layout projection as a variable.

--- us_per_capita.py
+++ us_per_capita.py
@@ -69,15 +69,11 @@ data = [dict(
     )
 )]
 
+layout_projection = dict(type='albers usa')
 layout = dict(
     title='Terrorist Attacks per 100,000 People in United States (1970-2015)',
-    geo=dict(
-        countrycolor='rgb(255, 255, 255)',
-        projection=dict(type='albers usa'),
-        showlakes=True,
-        lakecolor='rgb(255, 255, 255)'),
-        scope='usa'
-)
+    geo=dict(countrycolor='rgb(255, 255, 255)',lakecolor='rgb(255, 255, 255)'),
+        projection=layout_projection,scope='usa',showlakes=True)
 
 figure = dict(data=data, layout=layout)
 plot(figure)
