27a9014d1bf060d1c3e5c2bbe60f71ffc1a5c501
Mike DeLong <mikedelong@outlook.com>
2017-12-28 09:47:04 -0500

    feature: this should be the first working version of the global animated view once I can get the basemap install working.

--- global_animation.py
+++ global_animation.py
@@ -3,8 +3,10 @@
 import pandas as pd
 
 # import numpy as np
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.basemap import Basemap
+
 # import matplotlib.animation as animation
 # from IPython.display import HTML
 # import warnings
@@ -18,5 +20,27 @@ try:
     regions = list(set(t_file.region_txt))
     colors = ['yellow', 'red', 'lightblue', 'purple', 'green', 'orange', 'brown', 'aqua', 'lightpink', 'lightsage',
               'lightgray', 'navy']
+    plt.figure(figsize=(15, 8))
+    m = Basemap(projection='mill', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180, lat_ts=20,
+                resolution='c')
+    m.drawcoastlines()
+    m.drawcountries()
+    m.fillcontinents(color='burlywood', lake_color='lightblue', zorder=1)
+    m.drawmapboundary(fill_color='lightblue')
+
+
+    def pltpoints(region, color=None, label=None):
+        x, y = m(list(t_file.longitude[t_file.region_txt == region].astype("float")),
+                 (list(t_file.latitude[t_file.region_txt == region].astype("float"))))
+        points = m.plot(x, y, "o", markersize=4, color=color, label=label, alpha=.5)
+        return (points)
+
+
+    for i, region in enumerate(regions):
+        pltpoints(region, color=colors[i], label=region)
+
+    plt.title("Global Terrorism (1970 - 2015)")
+    plt.legend(loc='lower left', prop={'size': 11})
+    plt.show()
 except:
     print('File load: Failed')
