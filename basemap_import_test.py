c374b396c4a9cd189e21be8606e84181730e923e
Mike DeLong <mikedelong@outlook.com>
2017-12-28 09:57:32 -0500

    new file: simple test for importing matplotlib basemap.

---
+++ basemap_import_test.py
@@ -0,0 +1,20 @@
+
+import logging
+
+# set up logging
+formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+console_handler.setLevel(logging.DEBUG)
+logger.debug('started')
+
+try:
+    from mpl_toolkits.basemap import Basemap
+except Exception as error:
+    logger.warning(error)
+
+logger.debug('success!')
+
